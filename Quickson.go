f0f94d62e9ad747f73668c11193d0ebcb5b961a5
Aloïs Laurent Boë <45016834+ithirzty@users.noreply.github.com>
2020-10-30 21:21:38 +0100

    Update Quickson.go
    Adaptation for interfaces that are not structs

--- Quickson.go
+++ Quickson.go
@@ -10,52 +10,61 @@ import (
 func Marshal(x interface{}) string {
 	v := reflect.ValueOf(x)
 	vi := reflect.Indirect(v)
-	var t = "{"
-	for i := 0; i != vi.NumField(); i++ {
-		osi := vi.Field(i).Type().String()
-		switch osi {
-		case "string":
-			t += "\"" + vi.Type().Field(i).Name + "\":\"" + strings.Replace(fmt.Sprint(vi.Field(i).Interface()), "\"", "\\\"", -1) + "\","
-		case "bool", "int", "uint8":
-			t += "\"" + vi.Type().Field(i).Name + "\":\"" + fmt.Sprint(vi.Field(i).Interface()) + "\","
-		default:
-			if compareBytes(osi[:4], "map[") {
-				t += "\"" + vi.Type().Field(i).Name + "\":{"
-				mapTmpKeys := reflect.ValueOf(vi.Field(i).Interface()).MapKeys()
-				for _, key := range mapTmpKeys {
-					switch reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Type().String() {
-					case "int", "uint8", "bool":
-						t += "\"" + fmt.Sprint(key.Interface()) + "\":" + fmt.Sprint(reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Interface()) + ","
-					case "string":
-						t += "\"" + fmt.Sprint(key.Interface()) + "\":\"" + fmt.Sprint(reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Interface()) + "\","
-					default:
-						t += "\"" + fmt.Sprint(key.Interface()) + "\":\"" + marshalDeep(reflect.ValueOf(reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Interface()), reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Type().String()) + ","
+	var t string = ""
+	if compareBytes(vi.Type().String()[:4], "map[") || compareBytes(vi.Type().String()[:1], "[") {
+		t = marshalDeep(vi, vi.Type().String())
+	} else {
+		t = "{"
+		for i := 0; i != vi.NumField(); i++ {
+			osi := vi.Field(i).Type().String()
+			switch osi {
+			case "string":
+				t += "\"" + vi.Type().Field(i).Name + "\":\"" + strings.Replace(fmt.Sprint(vi.Field(i).Interface()), "\"", "\\\"", -1) + "\","
+			case "bool", "int", "uint8":
+				t += "\"" + vi.Type().Field(i).Name + "\":\"" + fmt.Sprint(vi.Field(i).Interface()) + "\","
+			default:
+				if compareBytes(osi[:4], "map[") {
+					t += "\"" + vi.Type().Field(i).Name + "\":{"
+					mapTmpKeys := reflect.ValueOf(vi.Field(i).Interface()).MapKeys()
+					for _, key := range mapTmpKeys {
+						switch reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Type().String() {
+						case "int", "uint8", "bool":
+							t += "\"" + fmt.Sprint(key.Interface()) + "\":" + fmt.Sprint(reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Interface()) + ","
+						case "string":
+							t += "\"" + fmt.Sprint(key.Interface()) + "\":\"" + fmt.Sprint(reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Interface()) + "\","
+						default:
+							t += "\"" + fmt.Sprint(key.Interface()) + "\":\"" + marshalDeep(reflect.ValueOf(reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Interface()), reflect.ValueOf(vi.Field(i).Interface()).MapIndex(key).Type().String()) + ","
+						}
 					}
-				}
-				t = t[:len(t)-1]
-				t += "},"
-			} else if compareBytes(osi[:1], "[") {
-				t += "\"" + vi.Type().Field(i).Name + "\":["
-				for ia := 0; ia < reflect.ValueOf(vi.Field(i).Interface()).Len(); ia++ {
-					switch reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Type().String() {
-					case "int", "uint8", "bool":
-						t += fmt.Sprint(reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Interface()) + ","
-					case "string":
-						t += "\"" + fmt.Sprint(reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Interface()) + "\","
-					default:
-						t += marshalDeep(reflect.ValueOf(reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Interface()), reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Type().String()) + ","
+					t = t[:len(t)-1]
+					t += "},"
+				} else if compareBytes(osi[:1], "[") {
+					t += "\"" + vi.Type().Field(i).Name + "\":["
+					for ia := 0; ia < reflect.ValueOf(vi.Field(i).Interface()).Len(); ia++ {
+						switch reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Type().String() {
+						case "int", "uint8", "bool":
+							t += fmt.Sprint(reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Interface()) + ","
+						case "string":
+							t += "\"" + fmt.Sprint(reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Interface()) + "\","
+						default:
+							t += marshalDeep(reflect.ValueOf(reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Interface()), reflect.ValueOf(vi.Field(i).Interface()).Index(ia).Type().String()) + ","
+						}
 					}
+					t = t[:len(t)-1]
+					t += "],"
+				} else {
+					t += Marshal(vi.Field(i).Interface())
 				}
-				t = t[:len(t)-1]
-				t += "],"
-			} else {
-				t += Marshal(vi.Field(i).Interface())+","
 			}
 		}
+		t = t[:len(t)-1]
+		t += "}"
 	}
-	t = t[:len(t)-1]
-	return t + "}"
+	return t
 }
+
+//to compare types efficiently
+
 func compareBytes(sa string, sb string) bool {
 	a := []byte(sa)
 	b := []byte(sb)
@@ -66,6 +75,9 @@ func compareBytes(sa string, sb string) bool {
 	}
 	return true
 }
+
+//to iterate deeply in the interface, can take map and slices/array. Interfaces are redirected to Marshal
+
 func marshalDeep(vi reflect.Value, bytedType string) string {
 	var t string = ""
 	if compareBytes(bytedType[:4], "map[") {
@@ -103,3 +115,8 @@ func marshalDeep(vi reflect.Value, bytedType string) string {
 	}
 	return t
 }
+
+//[IN COMMING]Unmarshal is used to transform JSON into an interface. You will need to input the pointer of the interface to fill.
+// func Unmarshal(j string, x *interface{}) {
+
+// }
